099f56d04054a1abd61e2c9fa05dcb863a8e1c02
Marina Sudakova <rubbish_bin@inbox.ru>
2022-09-27 12:46:06 +0500

    Task3

---
+++ Lesson_2/Task_3.py
@@ -0,0 +1,7 @@
+# 3. Напишите программу, в которой пользователь будет задавать две строки,
+#    программа - определять количество вхождений одной строки в другой.
+
+string_1 = input('Write a first string: ')
+string_2 = input('Write a second string: ')
+
+print(string_1.count(string_2))
