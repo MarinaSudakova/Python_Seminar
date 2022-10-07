d5a784d2946fc04a07d0bf359c57565631af0e26
Marina Sudakova <rubbish_bin@inbox.ru>
2022-10-07 12:38:27 +0500

    task5

---
+++ Lesson_4/HOMEWORK/Task_1_tverc.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+
+
+def accuracy(num, acc):
+    number = Decimal(f"{num}")
+    return number.quantize(Decimal(f"{acc}"))
+
+
+print(accuracy(float(input("Enter a real number: ")), float(input("Enter the required accuracy 0.0001: "))))
+
+# или
+
+num = float(input('Enter a real number: '))
+
+_, accu = input("Enter the required accuracy '0.0001': ").split(".")
+print(f"{num:.{len(accu)}f}")
