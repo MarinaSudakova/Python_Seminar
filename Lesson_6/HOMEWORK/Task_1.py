d82612242cdfa76024543e509389f949284f69dd
Marina Sudakova <rubbish_bin@inbox.ru>
2022-10-16 20:01:21 +0500

    HW1

---
+++ Lesson_6/HOMEWORK/Task_1.py
@@ -0,0 +1,20 @@
+# Представлен список чисел. Необходимо вывести элементы исходного списка, 
+# значения которых больше предыдущего элемента. Use comprehension.
+
+from random import choices
+# def sorted_list(numbers: list):
+#     new_list = []
+#     for x in range(1, len(numbers)):
+#         if numbers[x] > numbers[x-1]:
+#             new_list.append(numbers[x])
+#     return new_list
+
+list_num = choices(range(1, 20), k=9)
+print(list_num)
+
+
+# list_num = sorted_list([8, 18, 14, 7, 15, 12, 0, 9, 16])
+# print(list_num)
+
+sorted_list = [list_num[x] for x in range(1, len(list_num)) if list_num[x] > list_num[x-1]]
+print(sorted_list)
